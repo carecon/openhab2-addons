fd57803f00ef104d37ac3431fabdf9dea188e73e
Dimalo <dima.loschkin@gmail.com>
2017-04-10 07:40:46 +0200

    not needed anymore

--- addons/binding/org.openhab.binding.mihome/src/main/java/org/openhab/binding/mihome/internal/XiaomiHandlerFactory.java
+++ addons/binding/org.openhab.binding.mihome/src/main/java/org/openhab/binding/mihome/internal/XiaomiHandlerFactory.java
@@ -95,37 +95,35 @@ public class XiaomiHandlerFactory extends BaseThingHandlerFactory {
     protected ThingHandler createHandler(Thing thing) {
         ThingTypeUID thingTypeUID = thing.getThingTypeUID();
 
-        if (XiaomiBridgeHandler.SUPPORTED_THING_TYPES.contains(thingTypeUID)) {
+        if (thingTypeUID.equals(THING_TYPE_BRIDGE)) {
             XiaomiBridgeHandler handler = new XiaomiBridgeHandler((Bridge) thing);
             registerItemDiscoveryService(handler);
             return handler;
-        } else if (XiaomiDeviceBaseHandler.SUPPORTED_THING_TYPES.contains(thingTypeUID)) {
-            if (thingTypeUID.equals(THING_TYPE_GATEWAY)) {
-                return new XiaomiActorGatewayHandler(thing);
-            } else if (thingTypeUID.equals(THING_TYPE_SENSOR_HT)) {
-                return new XiaomiSensorHtHandler(thing);
-            } else if (thingTypeUID.equals(THING_TYPE_SENSOR_MOTION)) {
-                return new XiaomiSensorMotionHandler(thing);
-            } else if (thingTypeUID.equals(THING_TYPE_SENSOR_SWITCH)) {
-                return new XiaomiSensorSwitchHandler(thing);
-            } else if (thingTypeUID.equals(THING_TYPE_SENSOR_MAGNET)) {
-                return new XiaomiSensorMagnetHandler(thing);
-            } else if (thingTypeUID.equals(THING_TYPE_SENSOR_CUBE)) {
-                return new XiaomiSensorCubeHandler(thing);
-            } else if (thingTypeUID.equals(THING_TYPE_SENSOR_AQARA1)) {
-                return new XiaomiAqaraSensorSwitch1Handler(thing);
-            } else if (thingTypeUID.equals(THING_TYPE_SENSOR_AQARA2)) {
-                return new XiaomiAqaraSensorSwitch2Handler(thing);
-            } else if (thingTypeUID.equals(THING_TYPE_ACTOR_AQARA1)) {
-                return new XiaomiAqaraActorSwitch1Handler(thing);
-            } else if (thingTypeUID.equals(THING_TYPE_ACTOR_AQARA2)) {
-                return new XiaomiAqaraActorSwitch2Handler(thing);
-            } else if (thingTypeUID.equals(THING_TYPE_ACTOR_PLUG)) {
-                return new XiaomiActorPlugHandler(thing);
-            }
+        } else if (thingTypeUID.equals(THING_TYPE_GATEWAY)) {
+            return new XiaomiActorGatewayHandler(thing);
+        } else if (thingTypeUID.equals(THING_TYPE_SENSOR_HT)) {
+            return new XiaomiSensorHtHandler(thing);
+        } else if (thingTypeUID.equals(THING_TYPE_SENSOR_MOTION)) {
+            return new XiaomiSensorMotionHandler(thing);
+        } else if (thingTypeUID.equals(THING_TYPE_SENSOR_SWITCH)) {
+            return new XiaomiSensorSwitchHandler(thing);
+        } else if (thingTypeUID.equals(THING_TYPE_SENSOR_MAGNET)) {
+            return new XiaomiSensorMagnetHandler(thing);
+        } else if (thingTypeUID.equals(THING_TYPE_SENSOR_CUBE)) {
+            return new XiaomiSensorCubeHandler(thing);
+        } else if (thingTypeUID.equals(THING_TYPE_SENSOR_AQARA1)) {
+            return new XiaomiAqaraSensorSwitch1Handler(thing);
+        } else if (thingTypeUID.equals(THING_TYPE_SENSOR_AQARA2)) {
+            return new XiaomiAqaraSensorSwitch2Handler(thing);
+        } else if (thingTypeUID.equals(THING_TYPE_ACTOR_AQARA1)) {
+            return new XiaomiAqaraActorSwitch1Handler(thing);
+        } else if (thingTypeUID.equals(THING_TYPE_ACTOR_AQARA2)) {
+            return new XiaomiAqaraActorSwitch2Handler(thing);
+        } else if (thingTypeUID.equals(THING_TYPE_ACTOR_PLUG)) {
+            return new XiaomiActorPlugHandler(thing);
+        } else {
+            return null;
         }
-
-        return null;
     }
 
     @Override
